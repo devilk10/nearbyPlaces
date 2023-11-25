85bd112bf79cbbf00e59acb60399c32f29fbafe8
Ketan Sangle <ksangle24@gmail.com>
2023-11-25 11:38:10 +0530

    Fix locatio permission crash

--- app/src/main/java/com/ketansa/nearbyme/MainActivity.kt
+++ app/src/main/java/com/ketansa/nearbyme/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.ketansa.nearbyme
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,23 +9,20 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableDoubleStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
@@ -39,45 +35,49 @@ import com.ketansa.nearbyme.ui.NearbyPlacesScreen
 import com.ketansa.nearbyme.ui.theme.NearbyMeTheme
 
 class MainActivity : ComponentActivity() {
+
     private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private var PERMISSION_ID = 44
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-        setContent {
-            NearbyMeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    LocationScreen()
-                }
-            }
-        }
-    }
-
-    @Composable
-    private fun LocationScreen() {
-        var latitude by remember { mutableDoubleStateOf(12.971599) }
-        var longitude by remember { mutableDoubleStateOf(77.594566) }
-
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-                LaunchedEffect(true) {
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
                     getLastLocation { location ->
-                        latitude = location?.latitude ?: 12.971599
-                        longitude = location?.longitude ?: 77.594566
+                        setContent {
+                            location?.let {
+                                NearbyPlacesScreen(
+                                    nearbyPlacesVM = AppContainer.nearbyPlacesVM,
+                                    latitude = it.latitude,
+                                    longitude = it.longitude
+                                )
+                            } ?: run {
+                                ErrorScreen()
+                            }
+                        }
                     }
                 }
+            }
 
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp)
-                ) {
-                    Log.d("TAG-imptan", "LocationScreen: $latitude, $longitude")
-                    NearbyPlacesScreen(AppContainer.nearbyPlacesVM, latitude, longitude)
+        // Check and request permissions
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                getLastLocation { location ->
+                    setContent {
+                        // Launch NearbyPlacesScreen only when we have valid location
+                        location?.let {
+                            NearbyPlacesScreen(
+                                nearbyPlacesVM = AppContainer.nearbyPlacesVM,
+                                latitude = it.latitude,
+                                longitude = it.longitude
+                            )
+                        } ?: run {
+                            ErrorScreen()
+                        }
+                    }
                 }
             } else {
                 Toast.makeText(this, "Please turn on your location...", Toast.LENGTH_LONG).show()
@@ -88,8 +88,25 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @SuppressLint("MissingPermission")
+
     private fun getLastLocation(callback: (Location?) -> Unit) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
         mFusedLocationClient.lastLocation
             .addOnCompleteListener(this) { task ->
                 val location = task.result
@@ -101,7 +118,6 @@ class MainActivity : ComponentActivity() {
             }
     }
 
-    @SuppressLint("MissingPermission")
     private fun requestNewLocationData(callback: (Location?) -> Unit) {
         val mLocationRequest = LocationRequest.create().apply {
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
@@ -111,6 +127,23 @@ class MainActivity : ComponentActivity() {
         }
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
         mFusedLocationClient.requestLocationUpdates(mLocationRequest, object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
                 val mLastLocation = locationResult.lastLocation
@@ -131,17 +164,28 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun requestPermissions() {
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-                getLastLocation { location -> }
-            }
-        }.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
     }
 
     private fun isLocationEnabled(): Boolean {
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
                 locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+}
+
+@Composable
+fun ErrorScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Error: location permission not granted",
+        )
     }
 }
 
